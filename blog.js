045bd9100c39c3e3ca30fb3d9cbaa344047f835c
rendrakb <rendrakb@gmail.com>
2025-10-10 01:43:15 +0700

    working blog page & tweaks

--- blog.js
+++ blog.js
@@ -1,17 +1,17 @@
-  fetch('/assets/blog/content.json')
-    .then(response => response.json())
-    .then(blogData => {
-      const container = document.getElementById('blog-container');
-      blogData.forEach(blog => {
-        const blogHTML = `
+fetch("/assets/blog/content.json")
+  .then((response) => response.json())
+  .then((blogData) => {
+    const container = document.getElementById("blog-container");
+    blogData.forEach((blog) => {
+      const blogHTML = `
           <div class="blog-post" id="${blog.id}">
-            <h3>${blog.title}</h3>
             <p>${blog.date}</p>
+            <h3>${blog.title}</h3>
             <p>${blog.content}</p>
             <p>#${blog.tags}</p>
           </div>
           <hr class="divider">
         `;
-        container.innerHTML += blogHTML;
-      });
+      container.innerHTML += blogHTML;
     });
+  });
