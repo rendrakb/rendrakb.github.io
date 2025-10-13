3331d73756fb43cb935f6b31700b332483f8251a
Rendra Kusuma <76859734+rendrakb@users.noreply.github.com>
2025-10-14 03:30:56 +0700

    animation speed

--- ascii-port.js
+++ ascii-port.js
@@ -251,7 +251,7 @@ async function play(startFrame = 0) {
   for (let i = startFrame; i < frames; i++) {
     document.getElementById("output-port").innerHTML =
       '<font color="' + colour + '"><pre>' + n[i] + "</font></pre>";
-    await new Promise((r) => setTimeout(r, 30));
+    await new Promise((r) => setTimeout(r, 100));
   }
 
   hasStarted = true;
